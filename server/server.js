4feb8c8ec4285bd5511e6ca14ef980a524a2ade6
abraham perez reyes <abrahampre27@gmail.com>
2021-11-10 16:00:00 -0600

    chanpter 3

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
 // import ApolloServer
@@ -40,4 +41,13 @@ db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
+});
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
